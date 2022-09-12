98b9f7d6cba891a3e251bc26ad7841966ebd65f7
Alexander Avrov <a.avrov@gmail.com>
2022-09-12 14:53:28 +0300

    Изменен неймспейс апи_в1

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls', namespace='users')),
-    path('api/', include('api.v1.urls', namespace='api')),
+    path('api/', include('api.v1.urls', namespace='api_v1')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
