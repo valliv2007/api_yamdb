3d0b7cc8c0ad7a669e3d4cf3819d8a3050819865
Илья Снежко <valliv2007@ya.ru>
2022-10-22 18:47:52 +0300

    russian text changed

--- api_yamdb/api/v1/mixins.py
+++ api_yamdb/api/v1/mixins.py
@@ -3,4 +3,4 @@ from rest_framework import mixins, viewsets
 
 class GetPostDeleteViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
                            mixins.DestroyModelMixin, viewsets.GenericViewSet):
-    """Вьюсет для просмотра всех экземпляров, публикации и удаления"""
+    """Вьюсет для просмотра всех экземпляров,  публикации и удаления"""
